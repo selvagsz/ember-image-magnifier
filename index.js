b451121829bfa605ca47923e30b9eccd831537d9
selvaganesh.b <selvaganesh.b@zohocorp.com>
2016-02-22 08:41:17 +0530

    Fix dependencies issue

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-image-magnifier'
+  name: 'ember-image-magnifier',
+
+  included: function(app){
+    this._super.included.apply(this, app);
+    var emberTetherAddon = this.addons.filter(function(addon) {
+      return addon.name === 'ember-tether';
+    })[0];
+    emberTetherAddon.importBowerDependencies(app);
+  }
 };
